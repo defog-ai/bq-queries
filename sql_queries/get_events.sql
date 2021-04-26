6df87e52aba437c9b22a08516a1f53e67bcd8033
Rishabh Srivastava <rishabhsriv@gmail.com>
2021-04-26 13:01:41 +0800

    updated event query to fix bug

--- sql_queries/get_events.sql
+++ sql_queries/get_events.sql
@@ -23,10 +23,10 @@ FROM (
     url_path,
     country,
     province,
-    city
+    city,
     SPLIT(event1_values, "||") event{num}_values
   FROM `the-broadline.fsd.web_ingestion`
   WHERE DATETIME(_PARTITIONTIME) BETWEEN "{from_time}" AND "{to_time}"
 ) t1
 CROSS JOIN UNNEST(t1.event{num}_values) AS event
-GROUP BY client_id, date, hour, url_path, event;
+GROUP BY client_id, date, hour, url_path, device_type, session_referrer, country, province, city, event;
