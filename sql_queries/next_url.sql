57e85eb0396ed1dab21e571eaab2ff1f820f316b
Rishabh Srivastava <rishabhsriv@gmail.com>
2021-05-25 18:28:54 +0800

    updated queries to make sure everything is captured

--- sql_queries/next_url.sql
+++ sql_queries/next_url.sql
@@ -26,7 +26,7 @@ FROM (
   FROM
     `the-broadline.fsd.web_ingestion`
   WHERE
-    DATETIME(_PARTITIONTIME) BETWEEN "{from_time}" AND "{to_time}"
+    DATETIME(_PARTITIONTIME) >= "{from_time}" OR _PARTITIONTIME IS NULL
   ORDER BY
     session_id, session_hit_num
 ) AS t1
